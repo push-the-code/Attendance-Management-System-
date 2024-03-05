a76a3159a7de6b24dfab6281364ae93d45589084
PushTheCode <43913828+push-the-code@users.noreply.github.com>
2024-03-05 17:33:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
 import pywhatkit as pwk
 
 class Details:
-    
+
     def attendance (self):
         self.name = input("Enter the attendees name:")
         self.gender = input("Enter the gender:")
         self.phone = input("Enter the phone number(along with ISD code):")
         self.email = input("Enter the email address:")
         self.attendance = float(input("Enter the attendance:"))
-        
+
 class Attendance (Details):
-    
+
     def receive_attnd (self):
 
         det = [
             "Name: %s"%(self.name), "Gender: %s"%(self.gender), "Phone: %s"%(self.phone), "Email: %s"%(self.email), "Attendance: %.2f"%(self.attendance)
         ]
-        self.message = " \n".join(det)
-        print(self.message)
-        
+        self.message = "\n".join(det)
+
         if (self.attendance < 75):
-            print ("The attendance is low!")
+            self.msg = self.message + "\n\nThe attendance is low!"
         else:
-            print ("Attendance is up to date!")
-  
+            atnd_msg = "Your attendance is up to date!"
+            self.msg = self.message + "\n\nThe attendance is up to date!"
+
+        print("\n%s" %self.msg)
+
+
     def send_message (self):
-        pwk.sendwhatmsg_instantly(self.phone, "Attendance Report:\n%s"%self.message)
+        pwk.sendwhatmsg_instantly(self.phone, "Attendance Report:\n%s"%self.msg)
+
 
 adtc = Attendance()
 adtc.attendance()
